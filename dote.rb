4d2498dc79a0e72e46c5f9e3866656045baa2493
Pai314 <168731575+neipia271828@users.noreply.github.com>
2025-10-28 13:46:03 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -1,7 +1,7 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/kmc2400/homebrew-dote"
-  url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
+  homepage "https://github.com/neipia271828/homebrew-dote"
+  url "https://raw.githubusercontent.com/neipia271828/homebrew-dote/main/dote.c"
   sha256 "6d6d07a60f2f9144b87630f99ee0bda6c103bd0d9c9d31be42d2fce52b6a1f21"
 
   def install
