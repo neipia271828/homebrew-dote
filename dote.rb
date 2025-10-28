dfa288fbcc2dfc6665cdc893854b9e0d9988bb04
Pai314 <168731575+neipia271828@users.noreply.github.com>
2025-10-28 13:49:35 +0900

    1.0.0

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/neipia271828/homebrew-dote"
   url "https://raw.githubusercontent.com/neipia271828/homebrew-dote/main/dote.c"
   sha256 "6d6d07a60f2f9144b87630f99ee0bda6c103bd0d9c9d31be42d2fce52b6a1f21"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
